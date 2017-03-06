b9137515ec8fa387de8fba8cbcc4d87e1c06bb65
helix90 <helix90@gmail.com>
2017-03-06 15:11:35 -0800

    Stub out pages

--- proptrunk/proptrunk.py
+++ proptrunk/proptrunk.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/inventory')
+def inventort():
+    return 'Inventory'
+
+@app.route('/checkout')
+def checkout():
+    return 'Checkout'
+
+@app.route('/checkin')
+def checkin():
+    return 'Checkin'
+
+@app.route('/users')
+def user():
+    return 'Users'
+
+@app.route('/reports')
+def reports()
+    return 'Repoert'
+
 if __name__ == '__main__':
     app.run()
 
