674501e7a509bb24ba5ac90a8fd1156bf389073c
helix90 <helix90@gmail.com>
2017-03-06 18:35:22 -0800

    add templates

--- proptrunk/proptrunk.py
+++ proptrunk/proptrunk.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return render_template(index.html)
 
 @app.route('/inventory')
 def inventory():
