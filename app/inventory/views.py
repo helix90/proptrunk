ba241914e66277219a89c5edf649282dcc94e11f
helix90 <helix90@gmail.com>
2017-04-18 15:12:59 -0700

    Update

--- app/inventory/views.py
+++ app/inventory/views.py
@@ -1,3 +1,15 @@
 from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
+from . import admin
+from .. import db
+from ..models import Thing
+
+@inventory.route('/inventory', methods=['GET', 'POST'])
+@login_required
+def inventory():
+    # show current inventory list
+
+    items = Thing.query.all()
+    # render template
+    return render_template('inventory/inventory.html', items=items, title='Inventory')
