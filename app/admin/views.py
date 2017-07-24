a60665252f4edafacbe6e4c292eb184c8f379c0e
helix90 <helix90@gmail.com>
2017-07-24 04:10:44 -0700

    Refactor names

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,9 +2,9 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import admin
-from forms import DepartmentForm, EmployeeAssignForm, RoleForm
+from forms import VendorForm, EmployeeAssignForm, RoleForm
 from .. import db
-from ..models import Department, Employee, Role
+from ..models import Vendor, Employee, Role
 
 
 def check_admin():
@@ -13,100 +13,100 @@ def check_admin():
         abort(403)
 
 
-# Department Views
+# Vendor Views
 
 
 @admin.route('/vendors', methods=['GET', 'POST'])
 @login_required
-def list_departments():
+def list_dendors():
     """
     List all vendors
     """
     check_admin()
 
-    departments = Department.query.all()
+    dendors = Vendor.query.all()
 
     return render_template('admin/vendors/vendors.html',
-                           departments=departments, title="Departments")
+                           dendors=dendors, title="Vendors")
 
 
 @admin.route('/vendors/add', methods=['GET', 'POST'])
 @login_required
-def add_department():
+def add_dendor():
     """
-    Add a department to the database
+    Add a dendor to the database
     """
     check_admin()
 
-    add_department = True
+    add_dendor = True
 
-    form = DepartmentForm()
+    form = VendorForm()
     if form.validate_on_submit():
-        department = Department(name=form.name.data,
+        dendor = Vendor(name=form.name.data,
                                 description=form.description.data)
         try:
-            # add department to the database
-            db.session.add(department)
+            # add dendor to the database
+            db.session.add(dendor)
             db.session.commit()
-            flash('You have successfully added a new department.')
+            flash('You have successfully added a new dendor.')
         except:
-            # in case department name already exists
-            flash('Error: department name already exists.')
+            # in case dendor name already exists
+            flash('Error: dendor name already exists.')
 
         # redirect to vendors page
-        return redirect(url_for('admin.list_departments'))
+        return redirect(url_for('admin.list_dendors'))
 
-    # load department template
-    return render_template('admin/vendors/department.html', action="Add",
-                           add_department=add_department, form=form,
-                           title="Add Department")
+    # load dendor template
+    return render_template('admin/vendors/dendor.html', action="Add",
+                           add_dendor=add_dendor, form=form,
+                           title="Add Vendor")
 
 
 @admin.route('/vendors/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
-def edit_department(id):
+def edit_dendor(id):
     """
-    Edit a department
+    Edit a dendor
     """
     check_admin()
 
-    add_department = False
+    add_dendor = False
 
-    department = Department.query.get_or_404(id)
-    form = DepartmentForm(obj=department)
+    dendor = Vendor.query.get_or_404(id)
+    form = VendorForm(obj=dendor)
     if form.validate_on_submit():
-        department.name = form.name.data
-        department.description = form.description.data
+        dendor.name = form.name.data
+        dendor.description = form.description.data
         db.session.commit()
-        flash('You have successfully edited the department.')
+        flash('You have successfully edited the dendor.')
 
         # redirect to the vendors page
-        return redirect(url_for('admin.list_departments'))
+        return redirect(url_for('admin.list_dendors'))
 
-    form.description.data = department.description
-    form.name.data = department.name
-    return render_template('admin/vendors/department.html', action="Edit",
-                           add_department=add_department, form=form,
-                           department=department, title="Edit Department")
+    form.description.data = dendor.description
+    form.name.data = dendor.name
+    return render_template('admin/vendors/dendor.html', action="Edit",
+                           add_dendor=add_dendor, form=form,
+                           dendor=dendor, title="Edit Vendor")
 
 
 @admin.route('/vendors/delete/<int:id>', methods=['GET', 'POST'])
 @login_required
-def delete_department(id):
+def delete_dendor(id):
     """
-    Delete a department from the database
+    Delete a dendor from the database
     """
     check_admin()
 
-    department = Department.query.get_or_404(id)
-    db.session.delete(department)
+    dendor = Vendor.query.get_or_404(id)
+    db.session.delete(dendor)
     db.session.commit()
-    flash('You have successfully deleted the department.')
+    flash('You have successfully deleted the dendor.')
 
     # redirect to the vendors page
-    return redirect(url_for('admin.list_departments'))
+    return redirect(url_for('admin.list_dendors'))
 
-    return render_template(title="Delete Department")
+    return render_template(title="Delete Vendor")
 
 
 # Role Views
@@ -222,23 +222,23 @@ def list_employees():
 @login_required
 def assign_employee(id):
     """
-    Assign a department and a role to an employee
+    Assign a dendor and a role to an employee
     """
     check_admin()
 
     employee = Employee.query.get_or_404(id)
 
-    # prevent admin from being assigned a department or role
+    # prevent admin from being assigned a dendor or role
     if employee.is_admin:
         abort(403)
 
     form = EmployeeAssignForm(obj=employee)
     if form.validate_on_submit():
-        employee.department = form.department.data
+        employee.dendor = form.dendor.data
         employee.role = form.role.data
         db.session.add(employee)
         db.session.commit()
-        flash('You have successfully assigned a department and role.')
+        flash('You have successfully assigned a dendor and role.')
 
         # redirect to the roles page
         return redirect(url_for('admin.list_employees'))
