042b12402933dbecd0745c3be92c46e573c968fe
helix90 <helix90@gmail.com>
2017-08-28 19:28:44 -0700

    add item form

---
+++ app/inventory/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+
+from ..models import Thing
+
+
+class AddItem(FlaskForm):
+    """
+    Form for adding items to the database
+    """
+    barcode = StringField('Email', validators=[DataRequired()])
+    name = StringField('Username', validators=[DataRequired()])
+    description = StringField('First Name', validators=[DataRequired()])
+    owner = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField('Add')
