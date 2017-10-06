e61f783f3642b7c03b05fdf1a79aeb2e37359e34
helix90 <helix90@gmail.com>
2017-10-06 10:56:56 -0700

    update inventory model

--- app/models.py
+++ app/models.py
@@ -4,14 +4,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login_manager
 
 
-class Employee(UserMixin, db.Model):
+class People(UserMixin, db.Model):
     """
     Create an Employee table
     """
 
     # Ensures table will be named in plural and not in singular
     # as is the name of the model
-    __tablename__ = 'employees'
+    __tablename__ = 'people'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), index=True, unique=True)
@@ -19,7 +19,7 @@ class Employee(UserMixin, db.Model):
     first_name = db.Column(db.String(60), index=True)
     last_name = db.Column(db.String(60), index=True)
     password_hash = db.Column(db.String(128))
-    department_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
+    department_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     is_admin = db.Column(db.Boolean, default=False)
 
@@ -46,24 +46,23 @@ class Employee(UserMixin, db.Model):
     def __repr__(self):
         return '<Employee: {}>'.format(self.username)
 
-
 # Set up user_loader
 @login_manager.user_loader
 def load_user(user_id):
-    return Employee.query.get(int(user_id))
+    return People.query.get(int(user_id))
 
 
-class Vendor(db.Model):
+class Companies(db.Model):
     """
     Create a Vendor table
     """
 
-    __tablename__ = 'vendors'
+    __tablename__ = 'companies'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True)
     description = db.Column(db.String(200))
-    employees = db.relationship('Employee', backref='vendor',
+    employees = db.relationship('people', backref='vendor',
                                 lazy='dynamic')
 
     def __repr__(self):
@@ -80,7 +79,7 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True)
     description = db.Column(db.String(200))
-    employees = db.relationship('Employee', backref='role',
+    people = db.relationship('People', backref='role',
                                 lazy='dynamic')
 
     def __repr__(self):
@@ -100,9 +99,22 @@ class Thing(db.Model):
     name = db.Column(db.String(255))
     description = db.Column(db.String(1024))
     owner = db.Column(db.String(255))
-    bc_image = db.Column(db.String(1024))
-    thumbnail = db.Column(db.String(1024))
-    base_image = db.Column(db.String(1024))
+    barcode = db.relation('Images', backref='barcode',
+                          lazy='dynamic')
+    owner = db.relation('Company', backref='owner',
+                        lazy='dynamic')
 
     def __repr__(self):
         return '<Item>: {}'.format(self.name)
+
+class Images(db.Model):
+    """
+    Create an Image Table
+    for paths to the image files
+    for the images of Things
+    """
+
+    __tablename__ = 'image'
+
+    id = db.Column(db.INT, primary_key=True)
+    image_path = db.Column(db.String(2048))
