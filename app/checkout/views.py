76aafa15d2bf0d531da5585196bb3840e87ad498
helix90 <helix90@gmail.com>
2017-11-27 13:57:09 -0800

    Add checkout view

--- app/checkout/views.py
+++ app/checkout/views.py
@@ -1,3 +1,22 @@
 from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
+from . import inventory
+from .. import db
+from ..models import Thing
+
+@checkout.route('/checkout', methods=['GET', 'POST'])
+@login_required
+def inventory():
+    # shopping cart
+
+    items = Thing.query.all()
+    # render template
+    return render_template('checkout/item.html', items=items, title='Inventory')
+
+@checkout.route('/checkout', methods=['GET', 'POST'])
+@login_required
+def item():
+    # check out a single item
+    item = Thing.query()
+    return render_template('checkout/item.html', item=item, title='element')
