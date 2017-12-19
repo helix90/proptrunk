1020b7d839cb033181a7e42bced29a20c3f95227
helix90 <helix90@gmail.com>
2017-12-18 19:01:44 -0800

    add image list

--- app/inventory/views.py
+++ app/inventory/views.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_required
 
 from . import inventory
 from .. import db
-from ..models import Thing
+from ..models import Thing, Image
 
 
 @app.before_request
@@ -24,9 +24,10 @@ def inventory_list():
 
 @inventory.route('/inventory', methods=['GET', 'POST'])
 @login_required
-def item(itemid = None):
+def item(itemid=None):
     # show a single item
     # select by item number
     # how do we get the image list?
     item = Thing.query(id=itemid)
-    return render_template('inventory/item.html', item=item, title='element')
+    image_list = Image.query(item=itemid)
+    return render_template('inventory/item.html', item=item, image_list=image_list, title='element')
